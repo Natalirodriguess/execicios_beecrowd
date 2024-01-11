0bed337f36bdc2021bd9471e02e28c1237fbef9d
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2024-01-10 22:53:10 -0300

    [main] commit inicial

---
+++ src/main/java/com/beecrowd/exercicios/listaExercicios/ExercicioTriangule.java
@@ -0,0 +1,10 @@
+package com.beecrowd.exercicios.listaExercicios;
+
+import java.io.IOException;
+
+public class ExercicioTriangule {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
